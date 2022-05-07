ab4a6a8b3b78b7d5ef983971a01043157f70f51d
azu <azuciao@gmail.com>
2022-05-07 18:18:37 +0900

    fix padding

--- src/renderer.ts
+++ src/renderer.ts
@@ -19,24 +19,47 @@ declare const api: typeof API;
         const title = frameTitle.replace("EPUB Reader on your website.", "bi-epub-reader").replace("Published with Bibi", "bi-epub-reader");
         window.document.title = title;
     }, 1000)
-    const onLoadIframe = () => {
-        const iframeWindow = iframe.contentWindow as Window;
-        console.log("loaded");
-        iframeWindow.addEventListener("keyup", (event) => {
-            const key = event.key;
-            console.log(key);
-        });
-    }
-    const nextPage = () => {
+
+    const scrollNext = () => {
         const iframeWindow = iframe.contentWindow as any;
-        console.log("test")
-        if (iframeWindow.S["reader-view-mode"] === "paged") {
+        const viewerFrame = iframeWindow.document.querySelector(".loaded.current iframe") as HTMLIFrameElement | null;
+        if (iframeWindow.S["reader-view-mode"] === "vertical") {
+            if (viewerFrame) {
+                viewerFrame.contentWindow.scrollBy(0, 50);
+                iframeWindow.scrollBy(0, 50)
+            }
+        }
+    }
+    const scrollPrev = () => {
+        const iframeWindow = iframe.contentWindow as any;
+        const viewerFrame = iframeWindow.document.querySelector(".loaded.current iframe") as HTMLIFrameElement | null;
+        if (iframeWindow.S["regstader-view-mode"] === "vertical") {
+            if (viewerFrame) {
+                viewerFrame.contentWindow.scrollBy(0, -50);
+                iframeWindow.scrollBy(0, -50);
+            }
         }
     }
     window.addEventListener("keydown", (event) => {
         const key = event.key;
-        if (key === " ") {
-            nextPage();
+        if (key === "j") {
+            scrollNext();
+        } else if (key === "k") {
+            scrollPrev()
         }
     });
+    const onLoadIframe = () => {
+        const iframeWindow = iframe.contentWindow as Window;
+        console.log("loaded");
+        iframeWindow.addEventListener("keydown", (event) => {
+            const key = event.key;
+            console.log("key", key);
+            if (key === "j") {
+                scrollNext();
+            } else if (key === "k") {
+                scrollPrev()
+            }
+        });
+    }
+    iframe.addEventListener("load", onLoadIframe)
 })()
